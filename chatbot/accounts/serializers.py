ee48dddd4d1227e75a3a9bd2956c70517946dc43
pshn111 <spei@ualberta.ca>
2019-05-24 16:34:54 -0600

    finish back end authentication

--- chatbot/accounts/serializers.py
+++ chatbot/accounts/serializers.py
@@ -19,8 +19,15 @@ class RegisterSerializer(serializers.ModelSerializer):
         user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
 
         return user
+
 # Login Serializer
-class LoginSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'email', 'password')
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+
+        raise serializers.ValidationError("Incorrect Credentials")
